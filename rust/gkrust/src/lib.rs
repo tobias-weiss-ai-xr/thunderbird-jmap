74dec3b3cec70c52c13e613b05eb79b9dca362be
Pi Coding Agent <pi-coding-agent@users.noreply.github.com>
2026-07-28 10:01:11 +0200

    fix: link jmap_xpcom into gkrust for successful build
    - Add jmap_xpcom as dependency in gkrust/Cargo.toml
    - Add extern crate jmap_xpcom in gkrust/src/lib.rs to prevent dead-code
      elimination of the XPCOM bridge symbols
    - Track rust/gkrust/ files (Cargo.toml, moz.build, src/lib.rs) in git
    - Update .gitignore: remove blanket 'moz.build' ignore, ignore only
      the upstream /moz.build and mailnews/moz.build

---
+++ rust/gkrust/src/lib.rs
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Add new crates to Cargo.toml and build them here to get into gkrust/libxul
+extern crate gkrust_shared as _;
+
+extern crate ews_xpcom as _;
+extern crate graph_xpcom as _;
+extern crate jmap_xpcom as _;
+extern crate mailnews_string_glue as _;
+extern crate mailnews_ui_glue as _;
+extern crate sys_tray as _;
